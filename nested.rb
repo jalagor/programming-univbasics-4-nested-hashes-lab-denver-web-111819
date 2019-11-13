93d08d9d20169a9ceb33c97b034b5c66561a8fde
John Rogala <johnrossrogala@gmail.com>
2019-11-13 09:14:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,8 @@ def dennis_ritchies_language
         }
      }
      
-    puts programmer_hash[:dennis_ritchie][:languages]
+    dennis_ritchies_language = programmer_hash[:dennis_ritchie][:languages] 
+    puts dennis_ritchies_language
 end
 
 def adding_matz
